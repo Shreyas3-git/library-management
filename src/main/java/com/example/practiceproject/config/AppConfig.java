c4dd1ada72704ce942107fa0b5c2d40de9d6898f
Shreyas Shinde <83245241+Shreyas3-git@users.noreply.github.com>
2025-03-07 11:16:26 +0530

    Resolving MAVEN_BUILD_6

--- src/main/java/com/example/practiceproject/config/AppConfig.java
+++ src/main/java/com/example/practiceproject/config/AppConfig.java
@@ -15,10 +15,18 @@ public class AppConfig
     @Value("${app.notifications.twilio.auth-token}")
     private String authToken;
 
+    @Value("${security.jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${security.jwt.issuer-uri}")
+    private String issuerUri;
+
     @PostConstruct
     public void logTwilioConfig() {
         System.out.println("Twilio Account SID: " + accountSid);
         System.out.println("Twilio Service SID: " + serviceSid);
         System.out.println("Twilio Auth Token: " + authToken);
+        System.out.println("JWT Secret : "+jwtSecret);
+        System.out.println("IssuerUrl : "+issuerUri);
     }
 }
